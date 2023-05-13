20ea0d33597916b485d1ba5d22755212301ddc8a
alexf05 <76061340+alexf05@users.noreply.github.com>
2023-05-13 10:26:45 +0300

    driver constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TeleOP")
 public class TeleOp extends LinearOpMode {
@@ -18,9 +18,12 @@ public class TeleOp extends LinearOpMode {
         public Servo angle = null;
         public Servo gear_rack = null;
 
+        public ModernRoboticsI2cColorSensor colorSensor = null;
+
         public float travel = 0;
-        double servo_angle = 0;
-        double gear_pos = 0;
+        public double servo_angle = 0;
+        public double gear_pos = 0;
+        public int color_number = 0;
 
     @Override
     public void runOpMode() {
@@ -31,8 +34,10 @@ public class TeleOp extends LinearOpMode {
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
 
-//        angle = hardwareMap.get(Servo.class, "angle");
-//        gear_rack = hardwareMap.get(Servo.class, "gear_rack");
+        angle = hardwareMap.get(Servo.class, "angle");
+        gear_rack = hardwareMap.get(Servo.class, "gear_rack");
+
+        colorSensor = hardwareMap.get(ModernRoboticsI2cColorSensor.class, "color sensor");
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -48,20 +53,43 @@ public class TeleOp extends LinearOpMode {
         backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
-//        {
+        colorSensor.enableLed(true);
 
-//        }
         waitForStart();
         while(opModeIsActive() && !isStopRequested()) {
-            travel=-gamepad1.left_stick_y;
-            //goFoword(travel);
+            color_number=colorSensor.readUnsignedByte(ModernRoboticsI2cColorSensor.Register.COLOR_NUMBER);
+           /* travel=-gamepad1.left_stick_y;
             double strafe = gamepad1.left_stick_x*1.1;
             float turn;
             turn=gamepad1.right_trigger-gamepad1.left_trigger;
             mecanum_drive(strafe,travel,turn);
-            Telemetry_motors();
+            servo_angle+=gamepad1.right_stick_y*0.001;
+            if(gamepad1.dpad_up)servo_angle+=0.0002;
+            if(gamepad1.dpad_down)servo_angle-=0.0002;
+            if(servo_angle>=0.8)servo_angle=0.8;
+            if(servo_angle<=0)servo_angle=0;
+            angle.setPosition(servo_angle);
+            gear_pos+=gamepad1.right_stick_x*0.001;
+            if(gamepad1.dpad_left)gear_pos-=0.0002;
+            if(gamepad1.dpad_right)gear_pos+=0.0002;
+            if(gear_pos<=0)gear_pos=0;
+            if(gear_pos>=1)gear_pos=1;
+            gear_rack.setPosition(gear_pos);
+            Telemetry_Servos();*/
+
         }
 
+    }
+
+    public void Telemetry_sensor(){
+        telemetry.addData("color number", color_number);
+        telemetry.update();
+    }
+    public void Telemetry_Servos(){
+        telemetry.addData("angle1",servo_angle);
+        telemetry.addData("angle", angle.getPosition());
+        telemetry.addData("gear_rack", gear_rack.getPosition());
+        telemetry.update();
     }
     public void Telemetry_motors(){
         telemetry.addData("travel", travel);
